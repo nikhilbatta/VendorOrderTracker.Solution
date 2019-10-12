6502fa2d7085fe34198602717a08f686e85a3ba2
nikhilbatta <nbatta08@gmail.com>
2019-10-11 17:27:28 -0700

    adds funcitonality to view orders for specific Vendor

--- VendorOrderTracker/Controllers/OrderController.cs
+++ VendorOrderTracker/Controllers/OrderController.cs
@@ -6,13 +6,23 @@ namespace VendorOrderTracker.Controllers
 {
     public class OrderController : Controller
     {
+        [HttpGet("/vendor/{ID}/allorders")]
+        public ActionResult AllOrders(int ID)
+        {  
+            Vendor vendorOrders = Vendor.FindByID(ID);
+            return View(vendorOrders.listOfOrders);
+        }
         [HttpGet("/order/{ID}")]
         public ActionResult Order()
         {
             return View();
         }
-        [HttpPost("")]
-        public ActionResult()
+        [HttpPost("/order/new")]
+        public ActionResult OrderIntoVendor(string aOrderTitle, string aDescription, string aPrice, string aDateOfOrder)
+        {
+            Order newOrder = new Order(aOrderTitle, aDescription, aPrice, aDateOfOrder);
+            return RedirectToAction();
+        }
         
     }
 }
